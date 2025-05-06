9bca6c4ef44ce8a4f7ce4da580a9b5a3da6b524c
sansari <salman.ansari@hyland.com>
2025-05-06 22:53:47 +0530

    salsohrab20 |Circuit Breaker and Resiliency | Complete

---
+++ billing-service/src/main/java/com/pm/stack/billingservice/kafka/KafkaConsumer.java
@@ -0,0 +1,31 @@
+package com.pm.stack.billingservice.kafka;
+
+import billing.events.BillingAccountEvent;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "billing-account", groupId = "billing-service")
+    public void consumeEvent(byte[] event) {
+        try {
+            BillingAccountEvent billingAccountEvent = BillingAccountEvent.parseFrom(event);
+            log.info("Received billing account event : [PatientId ={}, PatientName={}, PatientEmail={}]",
+                    billingAccountEvent.getPatientId(),
+                    billingAccountEvent.getName(),
+                    billingAccountEvent.getEmail());
+
+            //check if the billing account exists for the patient, if not then create it
+
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error parsing BillingAccountEvent :{}", e.getMessage());
+        }
+    }
+
+}
--- patient-service/src/main/java/org/pm/patientservice/grpc/BillingServiceGrpcClient.java
+++ patient-service/src/main/java/org/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -3,8 +3,11 @@ package org.pm.patientservice.grpc;
 import billing.BillingRequest;
 import billing.BillingResponse;
 import billing.BillingServiceGrpc;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.pm.patientservice.kafka.KafkaProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,12 +17,13 @@ import org.springframework.stereotype.Service;
 public class BillingServiceGrpcClient {
 
     private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
+    private final KafkaProducer kafkaProducer;
     private BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
 
     public BillingServiceGrpcClient(
             @Value("${billing.service.address:localhost}") String serverAddress,
-            @Value("${billing.server.grpc.port:9001}") int serverPort
-    ) {
+            @Value("${billing.server.grpc.port:9001}") int serverPort,
+            KafkaProducer kafkaProducer) {
 
         log.info("Connecting to billing GRPC service at {} : {}", serverAddress, serverPort);
 
@@ -28,8 +32,16 @@ public class BillingServiceGrpcClient {
         ).usePlaintext().build();
 
         blockingStub = BillingServiceGrpc.newBlockingStub(managedChannel);
+        this.kafkaProducer = kafkaProducer;
     }
 
+    /*
+    * name - name of circuit breaker instance
+    * fallbackMethod - what method to use if the request fails
+    * retry - specifies to attempt the request 3 times before triggering circuit breaker
+    * */
+    @CircuitBreaker(name="billingService" , fallbackMethod = "billingFallback")
+    @Retry(name ="billingRetry")
     public BillingResponse createNewBillingAccount(String patientId, String name, String email) {
 
         BillingRequest billingRequest = BillingRequest
@@ -43,6 +55,24 @@ public class BillingServiceGrpcClient {
 
         log.info("Received response from billing service : {}", billingResponse);
         return billingResponse;
+
+    }
+
+    /*
+    * same signature as createBillingAccount
+    * t -> exception that cause the circuit to open
+    * */
+    public BillingResponse billingFallback(String patientId, String name, String email, Throwable t) {
+
+        log.warn("[CIRCUIT BREAKER]: Billing service is unavailable. Triggered " +
+                "fallback  :{}", t.getMessage());
+
+      kafkaProducer.sendBillingAccountEvent(patientId, name, email);
+
+        return BillingResponse.newBuilder()
+                .setAccountId("")
+                .setStatus("PENDING")
+                .build();
 
     }
 }
